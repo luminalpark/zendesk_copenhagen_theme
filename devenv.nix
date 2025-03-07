b5ccb7178555a24055739584db7354ef828e7127
Carlo Cecco <carlo.cecco@luminalpark.com>
2025-03-07 14:41:23 +0100

    test

---
+++ devenv.nix
@@ -0,0 +1,56 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  languages.javascript = {
+    enable = lib.mkDefault true;
+    package = lib.mkDefault pkgs.nodejs_20;
+  };
+
+  languages.javascript.yarn = {
+    enable = lib.mkDefault true;
+  };
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  processes = {
+    #silly-example.exec = "while true; do echo hello && sleep 1; done";
+    ping.exec = "ping localhost";
+  };
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
